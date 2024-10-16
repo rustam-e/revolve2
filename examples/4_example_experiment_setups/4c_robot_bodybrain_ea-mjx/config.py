3cc2f8738f35558caf4e860b310510ec6731815d
Eynaliyev <6816931+Eynaliyev@users.noreply.github.com>
2024-10-16 21:22:14 +0200

    Update configs values to be valid

--- examples/4_example_experiment_setups/4c_robot_bodybrain_ea-mjx/config.py
+++ examples/4_example_experiment_setups/4c_robot_bodybrain_ea-mjx/config.py
@@ -1,6 +1,7 @@
 """Configuration parameters for this example."""
 
+
 NUM_SIMULATORS = 1
-POPULATION_SIZE = 3
-OFFSPRING_SIZE = 2
+POPULATION_SIZE = 10
+OFFSPRING_SIZE = 5
 NUM_GENERATIONS = 2
--- examples/4_example_experiment_setups/4c_robot_bodybrain_ea/config.py
+++ examples/4_example_experiment_setups/4c_robot_bodybrain_ea/config.py
@@ -1,6 +1,6 @@
 """Configuration parameters for this example."""
 
 NUM_SIMULATORS = 1
-POPULATION_SIZE = 1
+POPULATION_SIZE = 10
 OFFSPRING_SIZE = 5
 NUM_GENERATIONS = 2
