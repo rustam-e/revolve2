af5976b312b11c1a2065c5f53c3f0cdae991a1f3
Eynaliyev <6816931+Eynaliyev@users.noreply.github.com>
2024-11-07 22:22:34 +0100

    more variants, more steps

--- mjx_profile_mvp.py
+++ mjx_profile_mvp.py
@@ -471,8 +471,8 @@ def main(simulations, max_processes=None):
     
     # variants = [32, 1024, 2056, 4096, 8192, 16384, 32768, 65536, 131072, 256000, 512000]
     # steps = [32, 100, 500, 1024, 2000]
-    variants = [32, 1024, 2056, 4096, 8192, 16384]
-    steps = [32, 100]
+    variants = [32, 1024, 2056, 4096, 8192, 16384,  32768, 65536, ]
+    steps = [32, 100, 500]
     results = []
 
     # Loop through each simulation
